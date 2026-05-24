1adfd6557d057a0c069c55f943d12ea74c1a13ad
Damián Búho <damian.buho@proton.me>
2026-05-23 22:25:41 -0300

    chore: drop strip-ansi, use inline regex

--- tests/cli/cli_tests.js
+++ tests/cli/cli_tests.js
@@ -6,7 +6,7 @@ const chai = require('chai')
 const cp = require('child_process')
 const fs = require('fs')
 const ServerMock = require('mock-http-server')
-const stripAnsi = require('strip-ansi')
+const stripAnsi = s => s.replace(/\x1B\[[0-9;]*m/g, '')
 const repolinter = require(path.resolve('.'))
 const expect = chai.expect
 
